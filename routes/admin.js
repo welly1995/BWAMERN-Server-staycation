c6819742885336e79baa25692d6c531d1e27f084
welly1995 <64908016+welly1995@users.noreply.github.com>
2021-08-16 22:07:13 +0700

    full project styacaction

---
+++ routes/admin.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
+const { uploadSingle, uploadMultiple } = require("../middlewares/multer");
+const auth = require("../middlewares/auth");
+
+router.get("/signin", adminController.viewSignin);
+router.post("/signin", adminController.actionSignin);
+router.use(auth);
+router.get("/logout", adminController.actionLogout);
+router.get("/dashboard", adminController.viewDashboard);
+
+// Router Category
+router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+// Akhir Router Category
+
+// Router Bank
+router.get("/bank", adminController.viewBank);
+router.post("/bank", uploadSingle, adminController.addBank);
+router.put("/bank", uploadSingle, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+// Akhir Router Bank
+
+// Router Item
+router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+router.put("/item/:id", uploadMultiple, adminController.editItem);
+router.delete("/item/:id/delete", adminController.deleteItem);
+// Akhir Router Item
+
+// Router detail item
+router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
+router.post("/item/add/feature", uploadSingle, adminController.addFeature);
+router.put("/item/update/feature", uploadSingle, adminController.editFeature);
+router.delete("/item/:itemId/feature/:id", adminController.deleteFeature);
+// akhir Router detail item
+
+// Router Activity
+router.post("/item/add/activity", uploadSingle, adminController.addActivity);
+router.put("/item/update/activity", uploadSingle, adminController.editActivity);
+router.delete("/item/:itemId/activity/:id", adminController.deleteActivity);
+// Akhir Router Activity
+
+// Router Booking
+router.get("/booking", adminController.viewBooking);
+router.get("/booking/:id", adminController.showDetailBooking);
+router.put("/booking/:id/confirmation", adminController.actionConfirmation);
+router.put("/booking/:id/reject", adminController.actionReject);
+// Akhir Router Booking
+module.exports = router;
---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const apiController = require("../controllers/apiController");
+const { uploadSingle } = require("../middlewares/multer");
+const auth = require("../middlewares/auth");
+
+router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
+router.post("/booking-page", uploadSingle, apiController.bookingPage);
+
+module.exports = router;
